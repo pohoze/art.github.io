2fe6dcc9d255d7814f6250654d20a14fabaebf14
Alexandr Gorelenkov <31740602+bysany@users.noreply.github.com>
2019-02-05 09:06:07 +0300

    Update contactform.js

--- js/contactform.js
+++ js/contactform.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function ($) {
 
         $.ajax({
             type: "POST",
-            url: "http://bysany.art/index.php",
+            url: "http://bysany.art/contact.php",
             data: str,
             success: function (msg) {
                 if (msg == 'OK') {
