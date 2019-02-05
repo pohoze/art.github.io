132b58fee6b4b68b2bdaee2b890c86ab98ad108a
Alexandr Gorelenkov <31740602+bysany@users.noreply.github.com>
2019-02-05 08:32:31 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -43,13 +43,10 @@ if ($post) {
 
         $name_tema = "=?utf-8?b?" . base64_encode($name) . "?=";
 
-        $subject = "Новая заявка с сайта domain.name";
+        $subject = "Новая заявка с сайта bysany.art";
         $subject1 = "=?utf-8?b?" . base64_encode($subject) . "?=";
 
         $message = "\n\nСообщение: " . $message . "\n\nИмя: " . $name . "\n\nТелефон: " . $tel . "\n\n";
-
-        $message1 = "\n\nИмя: " . $name . "\n\nТелефон: " . $tel . "\n\nE-mail: " . $email . "\n\nСообщение: " . $message . "\n\n";
-
 
         $header = "Content-Type: text/plain; charset=utf-8\n";
 
